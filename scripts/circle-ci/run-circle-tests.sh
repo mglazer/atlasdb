7aa556480a6ee7babb0b92b65637edfd8a3eed11
John Boreiko <jboreiko@users.noreply.github.com>
2016-04-25 13:28:46 +0100

    Update run-circle-tests.sh

--- scripts/circle-ci/run-circle-tests.sh
+++ scripts/circle-ci/run-circle-tests.sh
@@ -4,7 +4,7 @@ CASSANDRA=':atlasdb-cassandra-tests:check'
 SHARED=':atlasdb-tests-shared:check'
 
 case $CIRCLE_NODE_INDEX in
-    0) ./gradlew --parallel check -x $CASSANDRA -x $SHARED ;;
+    0) ./gradlew --parallel --continue check -x $CASSANDRA -x $SHARED ;;
     1) ./gradlew --parallel $CASSANDRA ;;
     2) ./gradlew --parallel $SHARED ;;
 esac
